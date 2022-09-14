24f0e34b7d2072cb9e6851f68e3ff9b60963a557
hanool <29415530+hanool@users.noreply.github.com>
2022-09-14 13:14:55 +0900

    Add save&reload user-data using localStroage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -7,22 +7,55 @@ const movieSelect = document.getElementById('movie')
 
 let ticketPrice = +movieSelect.value // using +sign to convert value to Number type
 
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+  if (selectedSeats && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+  if (selectedMovieIndex) {
+    movieSelect.selectedIndex = selectedMovieIndex
+    ticketPrice = +movieSelect.value
+  }
+}
+
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
 const updateSelectedCount = () => {
-  const selectedSeatsCount = document.querySelectorAll('.row .seat.selected').length
-  
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+  const selectedSeatsCount = selectedSeats.length
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
   count.innerText = selectedSeatsCount
   total.innerText = selectedSeatsCount * ticketPrice
 }
 
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value)
   updateSelectedCount()
 });
 
 container.addEventListener('click', e => {
-  if (e.target.classList.contains('seat') 
-    && !e.target.classList.contains('occupied')) {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected')
     updateSelectedCount()
   }
 });
+
+window.addEventListener('load', function() {
+  populateUI()
+  updateSelectedCount()
+})
