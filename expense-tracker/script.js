f0304b284f01ac45e471393e80915d20f3aea94b
hanool <29415530+hanool@users.noreply.github.com>
2022-09-28 10:25:02 +0900

    Implement functions to calculate total balance

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -30,10 +30,47 @@ const addTransactionToDOM = (transaction) => {
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
   item.innerHTML = `
     ${transaction.text}<span>${sign}$${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn">x</button>
   `
 
   list.appendChild(item)
 }
 
+const getTotalIncome = () => {
+  return transactions
+    .map((transaction) => Number(transaction.amount))
+    .filter((amount) => amount > 0)
+    .reduce((prev, curr) => prev + curr, 0)
+}
+
+const getTotalExpense = () => {
+  return Math.abs(
+    transactions
+      .map((transaction) => Number(transaction.amount))
+      .filter((amount) => amount < 0)
+      .reduce((prev, curr) => prev + curr, 0)
+  )
+}
+
+const refreshTotal = () => {
+  const totalIncome = getTotalIncome().toFixed(2)
+  const totalExpense = getTotalExpense().toFixed(2)
+  const total = Math.abs(totalIncome - totalExpense).toFixed(2)
+  const sign = totalIncome - totalExpense < 0 ? '-' : '+'
+  money_plus.innerText = `+$${totalIncome}`
+  money_minus.innerText = `-$${totalExpense}`
+  balance.innerText = `${sign}$${total}`
+  if (total < 0) {
+    balance.classList.add('minus')
+  } else {
+    balance.classList.add('plus')
+  }
+}
+
 // Event Listeners
 window.addEventListener('load', init)
+
+const listChangeObserver = new MutationObserver(() => {
+  refreshTotal()
+})
+listChangeObserver.observe(list, { childList: true })
