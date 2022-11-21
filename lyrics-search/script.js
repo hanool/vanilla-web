bed0234032be32ef284c62032c1a299ed4ca397c
hanool <29415530+hanool@users.noreply.github.com>
2022-11-21 22:49:32 +0900

    Lyrics-search Add showing search result function

--- lyrics-search/script.js
+++ lyrics-search/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form')
 const searchTerm = document.getElementById('search-term')
 const btnSearch = document.getElementById('btn-search')
 const searchResult = document.getElementById('search-result')
-const btnMore = document.getElementById('btn-more')
+const pagination = document.getElementById('pagination')
 
 const apiURL = `https://api.lyrics.ovh`
 
@@ -15,7 +15,45 @@ const searchSongs = async (term) => {
 }
 
 const showData = (songData) => {
-  console.log(songData.data)
+  console.log(songData.prev || songData.next)
+  searchResult.innerHTML = `
+    <ul class="songs">
+      ${songData.data
+        .map(
+          (song) => `
+        <li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}"> Get Lyrics</button>
+        </li>
+      `
+        )
+        .join('')}
+    </ul>
+  `
+
+  if (songData.prev || songData.next) {
+    pagination.innerHTML = `
+      ${
+        songData.prev
+          ? `<button class="btn" onclick="getMoreSongs('${songData.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        songData.next
+          ? `<button class="btn" onclick="getMoreSongs('${songData.next}')">Next</button>`
+          : ''
+      }
+    `
+  } else {
+    pagination.innerHTML = ''
+  }
+}
+
+const getMoreSongs = async (url) => {
+  const res = await fetch(`${url}`)
+  const data = await res.json()
+
+  showData(data)
 }
 
 const submitSongSearchForm = (e) => {
